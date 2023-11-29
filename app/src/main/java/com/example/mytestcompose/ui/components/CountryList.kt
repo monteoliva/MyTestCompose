4ca9c5499edcf61c76336437e643208f1259f709
Claudio Monteoliva <luis.monteoliva@linkedby.com.br>
2023-11-29 16:15:40 -0300

    Update CountryList.kt

--- app/src/main/java/com/example/mytestcompose/ui/components/CountryList.kt
+++ app/src/main/java/com/example/mytestcompose/ui/components/CountryList.kt
@@ -1,15 +1,21 @@
 package com.example.mytestcompose.ui.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.mytestcompose.ui.theme.MyTestComposeTheme
 
 @Composable
 fun CountryList() {
@@ -47,5 +53,23 @@ fun CountryList() {
                 )
             }
         }
+    }
+}
+
+
+@OptIn(ExperimentalMaterialApi::class)
+@Preview(
+    name           = "Light Mode",
+    showBackground = true
+)
+@Preview(
+    uiMode         = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = false,
+    name           = "Dark Mode"
+)
+@Composable
+fun CountryListPreview() {
+    MyTestComposeTheme {
+        CountryList()
     }
 }
